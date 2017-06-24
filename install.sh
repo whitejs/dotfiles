be138647530bac0dccb0748ab04d986fe7d22a8d
Justin White <justinw@alumni.caltech.edu>
2017-06-24 01:17:02 -0700

    git prompt

--- install.sh
+++ install.sh
@@ -8,3 +8,9 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
+
+# fancy git prompt
+git clone https://github.com/magicmonty/bash-git-prompt.git $HOME/.bash-git-prompt --depth=1
+echo "GIT_PROMPT_ONLY_IN_REPO=1" >> $HOME/.bashrc
+echo "source ~/.bash-git-prompt/gitprompt.sh" >> $HOME/.bashrc
+
